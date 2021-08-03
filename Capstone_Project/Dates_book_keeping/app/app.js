a90455d57c0795f40bfd57f9e4d93b0c916f047c
Gonzalo Munilla Garrido <gonzalo.munilla-garrido@tum.de>
2021-08-03 19:21:10 +0200

    chnages with capstone

--- Capstone_Project/Dates_book_keeping/app/app.js
+++ Capstone_Project/Dates_book_keeping/app/app.js
@@ -11,14 +11,19 @@ const authTokens = {};
 const users = [
     // This user is added to the array to avoid creating new user on each restart
     {
-        firstName: 'gonzalo',
-        lastName: 'm',
-        email: 'example@gonzalo.com',
+        firstName: 'User_First_Name',
+        lastName: 'User_Last_Name',
+        email: 'user@email.com',
         // This is the SHA256 hash for value of `password`
         password: 'XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg='
     },
 ];
 
+// The app stores values
+var datelist = [];
+var todolist = [];
+
+// 4
 const getHashedPassword = (password) => {
     const sha256 = crypto.createHash('sha256');
     const hash = sha256.update(password).digest('base64');
@@ -26,7 +31,7 @@ const getHashedPassword = (password) => {
     return hash;
 }
 
-
+// 5
 const generateAuthToken = () => {
     return crypto.randomBytes(30).toString('hex');
 }
@@ -49,22 +54,30 @@ app.engine('hbs', exphbs({
 
 app.set('view engine', 'hbs');
 
+// 1 Get the home page
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// 2 Get the login part of the webpage - whose frontend is in login.hns and is hypereferenced
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
+// 3 This handles the input data from the post request of login.hbs
 app.post('/login', (req, res) => {
+    // there are 2 inputs we parse
     const { email, password } = req.body;
+    // for the authentication, we hash the value first
     const hashedPassword = getHashedPassword(password);
 
+    // we compare
     const user = users.find(u => {
         return u.email === email && hashedPassword === u.password
     });
 
+    // and then if successful we generate a token randomly  and save the login with cookies
+    // After that, we can send the user to protected.hbs where one can input the calendar date
     if (user) {
         const authToken = generateAuthToken();
 
@@ -81,6 +94,7 @@ app.post('/login', (req, res) => {
     }
 });
 
+// 6 
 app.get('/protected', (req, res) => {
     if (req.user) {
         res.render('protected');
@@ -92,29 +106,30 @@ app.get('/protected', (req, res) => {
     }
 });
 
-
+// 7
 app.post('/protected', (req, res) => {
-    const { date } = req.body;
+    const { date, text } = req.body;
     new Date
+    new Text
     console.log("Date entered: " + Date.parse(date))
+    console.log("Text entered: " + Text.parse(text))
+
+    // the app saves values for later retrieval
+    datelist.append(date)
+    todolist.append(text)
 
     const doSomething = new Promise((resolve, reject) => {
-        const request = require('request');
-        let url = process.env.BACKEND_API + (Date.parse(date)/1000)
-        let options = {
-            headers: {
-            }};
+        //const request = require('request');
+        //let url = process.env.BACKEND_API + (Date.parse(date)/1000)
+        //let options = {
+        //    headers: {
+         //   }};
 
-        request(url, options, (error, res, body) => {
-            if (error) {
-                console.log(error)
-                reject(error)
-            };
-
-            if (!error && res.statusCode == 200) {
-                // do something with JSON, using the 'body' variable
-                console.log(JSON.parse(res.body))
-            };
+        res.render(getData, {
+            post: {
+                date: datelist,
+                text: todolist
+            }
         });
     })
 });
--- Capstone_Project/README.md
+++ Capstone_Project/README.md
@@ -1,5 +1,6 @@
 # Capstone Project: 
 
+Without docker:
 
 cd into folder 
 npm install 
@@ -7,7 +8,7 @@ node app.js
 
 Then use your browser to visit localhost:3000
  
-It will ask you for a login, you should enter example@whatever.com and password as "password"
+It will ask you for a login, you should enter exactly: for email "user@email.com" and for password "password"
 
 Then the website will take you to a page to enter a date
 
@@ -16,4 +17,11 @@ You can choose a date
 Future dev:
 You can make modifications in app.js 
 You can add a new page to render the data if you like
+
+WIth docker:
+
+run on the directory where this file is contained
+docker-compose -f docker-compose-build.yaml build --parallel
+
+
 
